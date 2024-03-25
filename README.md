90d88a3e7f19833f72540b6318be2b038a39887c
glaubervila <glauber.vila.verde@gmail.com>
2024-03-24 21:47:11 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Airflow Devcontainer
+
+<!-- Configurar os lints baseado no repositorio do airflow -->
+<!-- https://github.com/apache/airflow/blob/main/.gitignore -->
+
+https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html
+
+
+Based on these articles
+
+https://medium.com/@paulmartins/debugging-airflow-with-vscode-21dbd41d4de6
+
+https://levelup.gitconnected.com/debugging-airflow-dags-in-containers-with-vs-code-e31c0e899c7e
+
+https://davidgriffiths-data.medium.com/debugging-airflow-in-a-container-with-vs-code-7cc26734444
+
+
+```bash
+gh repo clone glaubervila/airflow_devcontainer
+```
+
+```bash
+mkdir -p ./dags ./logs ./plugins ./config
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+```
+
+```bash
+docker compose up airflow-init
+```
+
+```bash
+docker compose up
+```
+
+
+### Export/Import Connections to Json file
+
+```bash
+airflow connections export src/config/connections.json
+```
+
+```bash
+airflow connections import src/config/connections.json
+```
+
---
+++ src/dags/twitter_dag.py
@@ -0,0 +1,65 @@
+import sys
+
+sys.path.append("/opt/airflow")
+
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import pendulum
+from airflow.models import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.dates import days_ago
+
+from include.operators.twitter_operator import TwitterOperator
+
+base_path = Path("/opt/airflow")
+data_path = base_path.joinpath("data")
+spark_scripts_path = base_path.joinpath("include/spark")
+
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+query = "datascience"
+end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
+start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
+
+with DAG(dag_id="TwitterDAG", start_date=pendulum.datetime(2024, 3, 23, tz="UTC"), schedule_interval="@daily") as dag:
+
+    # Teste celery task
+    tarefa_1 = EmptyOperator(task_id="tarefa_1")
+
+    # NEED Connection
+    # Type: HTTP
+    # ID: twitter_default
+    # Host: https://labdados.com
+    twitter_operator = TwitterOperator(
+        task_id="twitter_datascience",
+        file_path=data_path.joinpath(
+            f"twitter_datascience/extract_date={datetime.now().date()}/datascience_{datetime.now().date().strftime('%Y%m%d')}.json"
+        ),
+        query=query,
+        start_time=start_time,
+        end_time=end_time,
+    )
+
+    # NEED Connection
+    # Type: Spark
+    # ID: spark_default
+    # Host: local
+    # Deploy Mode: client
+    # Spark binary: spark-submit
+    twitter_transform = SparkSubmitOperator(
+        task_id="transform_twitter_datascience",
+        application=str(spark_scripts_path.joinpath("transformation.py")),
+        name="twitter_transformation",
+        application_args=[
+            "--src",
+            str(data_path.joinpath("twitter_datascience")),
+            "--dest",
+            str(data_path.joinpath("silver/twitter_datascience")),
+            "--process-date",
+            "{{ ds }}",
+        ],
+    )
+
+    tarefa_1 >> twitter_operator >> twitter_transform
